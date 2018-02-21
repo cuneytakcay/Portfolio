f73d51a2bc9b6d451493060ff2a0f81a10aedac3
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-02-21 13:40:03 -0700

    Added new images and normalize.css, modified homepage animations

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,72 +1,69 @@
-// Water ripple effect on the main page
-$('body').ripples({
-	imgUrl: null,
-	resolution: 500,
-	dropRadius: 10,
-	perturbance: 0.05,
-});
+var btnClicked = false;
 
-// Pulsating menu button
+// ------------------------------------------------- //
+// ------- jPulse & Links opening animations ------- //
+// ------------------------------------------------- //
 function pulsate(cond) {
-
 	$('#menu-btn').jPulse(cond);
 }
 
-var btnClicked = false;
+function openLinks() {
+	$('#menu-btn').css('opacity', '1');
+	$('.chevron').addClass('hide');
+	$('.tagline').css('opacity', '0');
+	$('#top-line').removeClass('undraw-top lines').addClass('draw-top lines');
+	$('#mid-line').removeClass('undraw-mid lines').addClass('draw-mid lines');
+	$('#bot-line').removeClass('undraw-bot lines').addClass('draw-bot lines');
+	$('#portfolio, #blog, #about-me').removeClass('close-links links').addClass('open-links links');
+	$('.line-title').css('opacity', '1');
+} 
+
+function closeLinks() {
+	$('#menu-btn').css('opacity', '0.3');
+	$('.chevron').removeClass('hide');
+	$('.tagline').css('opacity', '1');
+	$('#top-line').removeClass('draw-top lines').addClass('undraw-top lines');
+	$('#mid-line').removeClass('draw-mid lines').addClass('undraw-mid lines');
+	$('#bot-line').removeClass('draw-bot lines').addClass('undraw-bot lines');
+	$('#portfolio, #blog, #about-me').removeClass('open-links links').addClass('close-links links');
+	$('.line-title').css('opacity', '0');
+}
 
 $('#menu-btn').on('mouseenter', function() {
 	if (!btnClicked) {
-		$('.chevron').addClass('paused');
-		$('.tagline').addClass('tagline-opaque');
 		pulsate({
 			color: '#fff',
-			size: 75,
-			speed: 3000,
-			interval: 500,
+			size: 120,
+			speed: 2000,
+			interval: 400,
 			left: 0,
 			top: 50,
 			zIndex: -1
 		});
-		$('#horizontal').removeClass('line undraw-right').addClass('line draw-right');
-		$('#vertical').removeClass('line undraw-right').addClass('line draw-right');
-		$('#diagonal-long').removeClass('line undraw-long').addClass('line draw-long');
-		$('#diagonal-short').removeClass('line undraw-short').addClass('line draw-short');
+		openLinks();
 	}
 });
 
 $('#menu-btn').on('mouseleave', function() {
 	if (!btnClicked) {
-		$('.chevron').removeClass('paused');
-		$('.tagline').removeClass('tagline-opaque');
 		pulsate('disable');
-		$('#horizontal').removeClass('line draw-right').addClass('line undraw-right');
-		$('#vertical').removeClass('line draw-right').addClass('line undraw-right');
-		$('#diagonal-long').removeClass('line draw-long').addClass('line undraw-long');
-		$('#diagonal-short').removeClass('line draw-short').addClass('line undraw-short');
+		closeLinks();
 	}
 });
 
 $('#menu-btn').on('click', function() {
 	if (!btnClicked) {
 		btnClicked = true;
-		$('#menu-btn').css('opacity', '1');
-		$('.chevron').addClass('paused');
-		$('.tagline').addClass('tagline-opaque');
-		$('#horizontal').removeClass('draw-right undraw-right').addClass('line');
-		$('#vertical').removeClass('draw-right undraw-right').addClass('line');
-		$('#diagonal-long').removeClass('draw-long undraw-long').addClass('line');
-		$('#diagonal-short').removeClass('draw-short undraw-short').addClass('line');
+		openLinks();
 	} else {
 		btnClicked = false;
-		$('#menu-btn').css('opacity', '0.3');
-		$('.chevron').removeClass('paused');
-		$('.tagline').removeClass('tagline-opaque');
+		closeLinks();
 	}
 });
 
-
-
-// Animating chevron images
+// ------------------------------------------------- //
+// ----------- Animating chevron effects ----------- //
+// ------------------------------------------------- //
 var animateChevron = function() {
 	clearTimeout(animateChevron);
 
@@ -87,3 +84,13 @@ var animateChevron = function() {
 }
 
 setTimeout(animateChevron, 100);
+
+// ------------------------------------------------- //
+// ----- Water ripple effect on the main page. ----- //
+// ------------------------------------------------- //
+$('body').ripples({
+	imgUrl: null,
+	resolution: 500,
+	dropRadius: 10,
+	perturbance: 0.05,
+});
